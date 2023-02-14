7fb0197d8b4930d20fde9c33d7679beee1a82845
Matthew <hello@eightball.studio>
2023-02-14 16:04:27 -0700

    First 2 stages

---
+++ Assets/GameManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+[Serializable]
+public class Stage
+{
+    public string name;
+    [TextArea]
+    public string note;
+
+    [Header("The events called when the stage begins.")]
+    public UnityEvent events;
+}
+
+[Serializable]
+public class Objects
+{
+    public GameObject[] startEnabledObjects;
+    public GameObject[] startDisabledObjects;
+}
+
+public class GameManager : MonoBehaviour
+{
+    #region Environment
+    [ContextMenu("Reset Environment")]
+    public void ResetEnvironment()
+    {
+        foreach (GameObject go in objects.startEnabledObjects)
+        {
+            go.SetActive(true);
+        }
+        foreach (GameObject go in objects.startDisabledObjects)
+        {
+            go.SetActive(false);
+        }
+    }
+
+    public Objects objects;
+    #endregion
+
+    public List<Stage> stages = new List<Stage>();
+    private int stageIndex = 0;
+
+    public void _NextStage()
+    {
+        stageIndex++;
+        Stage stage = stages[stageIndex];
+
+        stage.events.Invoke();
+    }
+
+    public void _EnableObject(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+    public void _DisableObject(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+    public void _PlayAnimation(Animation anim)
+    {
+        anim.Play();
+    }
+
+    void Start()
+    {
+        ResetEnvironment();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
